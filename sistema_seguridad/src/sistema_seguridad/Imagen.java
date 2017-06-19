dfd599fe97d752bddb927b5d2821caa7d4f43899
MartinRodriguezG <martinrodriguezgervasio@gmail.com>
2017-06-18 21:28:34 -0300

    te es mi primer commit

--- sistema_seguridad/src/sistema_seguridad/Imagen.java
+++ sistema_seguridad/src/sistema_seguridad/Imagen.java
@@ -10,14 +10,53 @@ package sistema_seguridad;
  * @author federico
  */
 public class Imagen {
-    String idCamara; 
+    private String idCamara; 
     // Representa la cantidad de momentos que necesita el ProcesadorImagen para procesar esta imagen
-    Integer tiempoProcesamiento;
+    private Integer tiempoProcesamiento;
     // Representa el momento en que la imagen ingresa al sistema, este atributo 
     // debe ser modificado unicamente por una instancia de la clase GrupoCamara
-    Integer momentoGeneracion;
+    private Integer momentoGeneracion;
     // representa el momento en el que la imagen fue retirada del buffer de imagenes, para ser p
     // procesada
-    Integer momentoLeida;
+    private Integer momentoLeida;
+
+    public String getIdCamara() {
+        return idCamara;
+    }
+
+    public void setIdCamara(String idCamara) {
+        this.idCamara = idCamara;
+    }
+
+    public Integer getTiempoProcesamiento() {
+        return tiempoProcesamiento;
+    }
+
+    public void setTiempoProcesamiento(Integer tiempoProcesamiento) {
+        this.tiempoProcesamiento = tiempoProcesamiento;
+    }
+
+    public Integer getMomentoGeneracion() {
+        return momentoGeneracion;
+    }
+
+    public void setMomentoGeneracion(Integer momentoGeneracion) {
+        this.momentoGeneracion = momentoGeneracion;
+    }
+
+    public Integer getMomentoLeida() {
+        return momentoLeida;
+    }
+
+    public void setMomentoLeida(Integer momentoLeida) {
+        this.momentoLeida = momentoLeida;
+    }
+
+    public Imagen(String idCamara, Integer tiempoProcesamiento, Integer momentoGeneracion, Integer momentoLeida) {
+        this.idCamara = idCamara;
+        this.tiempoProcesamiento = tiempoProcesamiento;
+        this.momentoGeneracion = momentoGeneracion;
+        this.momentoLeida = momentoLeida;
+    }
     
 }
--- sistema_seguridad/src/sistema_seguridad/Notificador.java
+++ sistema_seguridad/src/sistema_seguridad/Notificador.java
@@ -5,15 +5,37 @@
  */
 package sistema_seguridad;
 
+import java.io.IOException;
+import java.util.logging.Level;
+
 /**
  *
  * @author federico
  */
 public class Notificador implements Runnable {
-
+    
     @Override
     public void run() {
-        System.out.println("Enviando notificaciones");
+        while(true){
+            try {
+                notificar();
+            } catch (IOException ex) {
+                java.util.logging.Logger.getLogger(Notificador.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+    private void notificar() throws IOException{
+        if(!Buffers.alertasANotificar.isEmpty())
+        {
+            Alerta notificacion = Buffers.alertasANotificar.poll();
+            notificacion.setMomentoNotificada(Reloj.getInstance().getMomentoActual());
+            Imagen imagen  = notificacion.getRequerido();
+            String idTablet = "D" + imagen.getIdCamara().substring(1, imagen.getIdCamara().length());
+            String texto = "Se ha enviado la alerta al dispositivo: "+idTablet;
+            Logger.getInstancia().log(texto);
+        }
+        
     }
     
 }
